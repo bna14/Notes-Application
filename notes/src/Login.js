d0f3dc9d229ad094045fa5a343b9d2a7471afbd7
bna14 <154318806+bna14@users.noreply.github.com>
2024-11-24 11:27:59 +0200

    Update Login.js

--- notes/src/Login.js
+++ notes/src/Login.js
@@ -7,7 +7,7 @@ function Login({ onLogin, onShowSignup }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://notes-application-x57d.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
